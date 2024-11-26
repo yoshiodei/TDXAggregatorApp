be1c751ff4a6dc3d36682e1fe4485285ac76dd8d
Yoshi Odei <yoshiodei@yahoo.com>
2024-11-26 11:54:20 +0000

    change community list

--- src/config/constant.js
+++ src/config/constant.js
@@ -6,178 +6,246 @@ export const mobileNetworks = {
 
 export const communityList = [
     {
-        "id": "1",
-        "name": "Samoa(Bongolo)",
-        "location": "Lambusie",
-        "region": "9",
-        "country": "Ghana"
-    },
-    {
         "id": "2",
         "name": "Suke",
-        "location": "Lambusie",
-        "region": "9",
-        "country": "Ghana"
+        "location": "Lambusie"
     },
     {
         "id": "6",
         "name": "Chum",
-        "location": "Lambusie",
-        "region": "9",
-        "country": "Ghana"
+        "location": "Lambusie"
     },
     {
         "id": "7",
         "name": "Billaw",
-        "location": "Lambusie",
-        "region": "9",
-        "country": "Ghana"
+        "location": "Lambusie"
     },
     {
         "id": "8",
-        "name": "Samoa(Gbal)",
-        "location": "Lambusie",
-        "region": "9",
-        "country": "Ghana"
-    },
-    {
-        "id": "9",
-        "name": "Samoa(Lung)",
-        "location": "Lambusie",
-        "region": "9",
-        "country": "Ghana"
-    },
-    {
-        "id": "10",
-        "name": "Samoa(Mwindiogang)",
-        "location": "Lambusie",
-        "region": "9",
-        "country": "Ghana"
-    },
-    {
-        "id": "11",
-        "name": "Samoa Naave",
-        "location": "Lambusie",
-        "region": "9",
-        "country": "Ghana"
+        "name": "Samoa",
+        "location": "Lambusie"
     },
     {
         "id": "12",
         "name": "Silbelle",
-        "location": "Sissala West ",
-        "region": "9",
-        "country": "Ghana"
+        "location": "Sissala West "
     },
     {
         "id": "13",
         "name": "Sorbelle",
-        "location": "Sissala West ",
-        "region": "9",
-        "country": "Ghana"
+        "location": "Sissala West "
     },
     {
         "id": "14",
-        "name": "Eremon (Bakyegan)",
-        "location": "Lawra",
-        "region": "9",
-        "country": "Ghana"
-    },
-    {
-        "id": "15",
-        "name": "Eremon (Bure)",
-        "location": "Lawra",
-        "region": "9",
-        "country": "Ghana"
-    },
-    {
-        "id": "16",
-        "name": "Eremon (Dabuoyiri)",
-        "location": "Lawra",
-        "region": "9",
-        "country": "Ghana"
-    },
-    {
-        "id": "17",
-        "name": "Eremon (Danko)",
-        "location": "Lawra",
-        "region": "9",
-        "country": "Ghana"
-    },
-    {
-        "id": "18",
-        "name": "Eremon (Dazzuri)",
-        "location": "Lawra",
-        "region": "9",
-        "country": "Ghana"
-    },
-    {
-        "id": "19",
-        "name": "Eremon (Kakaltuo)",
-        "location": "Lawra",
-        "region": "9",
-        "country": "Ghana"
-    },
-    {
-        "id": "20",
-        "name": "Eremon (Naayibog)",
-        "location": "Lawra",
-        "region": "9",
-        "country": "Ghana"
-    },
-    {
-        "id": "21",
-        "name": "Eremon (Naburinye)",
-        "location": "Lawra",
-        "region": "9",
-        "country": "Ghana"
-    },
-    {
-        "id": "22",
-        "name": "Eremon (Soriguon)",
-        "location": "Lawra",
-        "region": "9",
-        "country": "Ghana"
-    },
-    {
-        "id": "23",
-        "name": "Eremon (Toto)",
-        "location": "Lawra",
-        "region": "9",
-        "country": "Ghana"
-    },
-    {
-        "id": "24",
-        "name": "Eremon (Yagra)",
-        "location": "Lawra ",
-        "region": "9",
-        "country": "Ghana"
-    },
-    {
-        "id": "25",
-        "name": "Eremon (Zinpen)",
-        "location": "Lawra ",
-        "region": "9",
-        "country": "Ghana"
+        "name": "Eremon ",
+        "location": "Lawra"
     },
     {
         "id": "26",
         "name": "Ketuo",
-        "location": "Nandom",
-        "region": "9",
-        "country": "Ghana"
+        "location": "Nandom"
     },
     {
         "id": "27",
         "name": "Bu",
-        "location": "Nandom",
-        "region": "9",
-        "country": "Ghana"
+        "location": "Nandom"
     },
     {
-        "id": "28",
-        "name": "Eremon (Bompare)",
-        "location": "Lawra",
-        "region": "9",
-        "country": "Ghana"
+        "id": "29",
+        "name": "Pulema",
+        "location": "Sisala West"
+    },
+    {
+        "id": "30",
+        "name": "Jawia",
+        "location": "Sisala West"
+    },
+    {
+        "id": "32",
+        "name": "Gushegu ",
+        "location": "Karaga District"
+    },
+    {
+        "id": "33",
+        "name": "Zamashegu ",
+        "location": "Karaga District"
+    },
+    {
+        "id": "34",
+        "name": "Karaga ",
+        "location": "Karaga District"
+    },
+    {
+        "id": "35",
+        "name": "Pishegu",
+        "location": "Karaga District"
+    },
+    {
+        "id": "36",
+        "name": "Nyong nayili",
+        "location": "Karaga"
+    },
+    {
+        "id": "37",
+        "name": "Shenlanyili",
+        "location": "Karaga"
+    },
+    {
+        "id": "38",
+        "name": "Nankugna",
+        "location": "Gushegu"
+    },
+    {
+        "id": "39",
+        "name": "Nawuhugu",
+        "location": "Gushegu"
+    },
+    {
+        "id": "40",
+        "name": "Gunayili",
+        "location": "Gushegu"
+    },
+    {
+        "id": "41",
+        "name": "Dowine-Dompare",
+        "location": "Lawra"
+    },
+    {
+        "id": "42",
+        "name": "Kupulima",
+        "location": "Sissala West"
+    },
+    {
+        "id": "43",
+        "name": "Jeffisi",
+        "location": "Sisala West"
+    },
+    {
+        "id": "44",
+        "name": "Pulima",
+        "location": "Sissala West"
+    },
+    {
+        "id": "45",
+        "name": "Nyimati",
+        "location": "Sissala West"
+    },
+    {
+        "id": "46",
+        "name": "Nankpawie",
+        "location": "Sissala East"
+    },
+    {
+        "id": "47",
+        "name": "Sakai",
+        "location": "Sissala East"
+    },
+    {
+        "id": "48",
+        "name": "Kowie",
+        "location": "Sissala East"
+    },
+    {
+        "id": "49",
+        "name": "Lilixia",
+        "location": "Sissala East"
+    },
+    {
+        "id": "50",
+        "name": "Pieng",
+        "location": "Sissala East"
+    },
+    {
+        "id": "51",
+        "name": "Challu",
+        "location": "Sissala East"
+    },
+    {
+        "id": "52",
+        "name": "Tarsaw",
+        "location": "Sissala East"
+    },
+    {
+        "id": "53",
+        "name": "Bandei",
+        "location": "Sissala East"
+    },
+    {
+        "id": "54",
+        "name": "Sakalu",
+        "location": "Sissala East"
+    },
+    {
+        "id": "55",
+        "name": "Walembelle",
+        "location": "Sissala East"
+    },
+    {
+        "id": "56",
+        "name": "Chinchang",
+        "location": "Sissala East"
+    },
+    {
+        "id": "57",
+        "name": "Kongual",
+        "location": "Lambusie"
+    },
+    {
+        "id": "58",
+        "name": "Sentu(Tanyaaga)",
+        "location": "Lambusie"
+    },
+    {
+        "id": "59",
+        "name": "Piina(Zongo)",
+        "location": "Lambusie"
+    },
+    {
+        "id": "60",
+        "name": "Naawie ",
+        "location": "Lambusie"
+    },
+    {
+        "id": "61",
+        "name": "Gbarima",
+        "location": "Sissala West"
+    },
+    {
+        "id": "62",
+        "name": "Buu",
+        "location": "Lambusie "
+    },
+    {
+        "id": "63",
+        "name": "Hakyagagaun",
+        "location": "Lambusie"
+    },
+    {
+        "id": "64",
+        "name": "Tumu",
+        "location": "Sissala East"
+    },
+    {
+        "id": "65",
+        "name": "Kandia",
+        "location": "Sissala West"
+    },
+    {
+        "id": "66",
+        "name": "Jitong",
+        "location": "Sissala West"
+    },
+    {
+        "id": "67",
+        "name": "Bullu",
+        "location": "Sissala West"
+    },
+    {
+        "id": "68",
+        "name": "Zandua",
+        "location": "Karaga"
+    },
+    {
+        "last_synced": "2024-11-26 11:51:23"
     }
 ];
